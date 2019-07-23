cfe103a79ff60d7f755cae2cb81cf9b7f4b85244
nicolasgover <47012483+nicolasgover@users.noreply.github.com>
2019-07-23 10:34:53 -0500

    Add files via upload

--- ScanID.py
+++ ScanID.py
@@ -1,4 +1,3 @@
-import numpy as np
 import imutils
 import pytesseract
 import os
@@ -7,10 +6,11 @@ import sys
 import math
 from PIL import Image, ImageOps
 from unidecode import unidecode
+from pathlib import Path
 import TemplateData as templates
 import Document as document
 import Transform as transform
-
+import numpy as np
 
 # TODO -- GOALS -- TODO
 # 1. Better Template
@@ -23,8 +23,8 @@ import Transform as transform
 #global variable declaration
 GOOD_MATCH_PERCENT = .15
 SRC_PATH = "/Users/ngover/Documents/TestPrograms/Images/"
-IMG = "Texas/H/TX_H_test19.png"
-BLUR_THRESHOLD = 50 # TODO mess around with this
+IMG = "Samples/TX_H_test35.png"
+BLUR_THRESHOLD = 40 # TODO mess around with this
 DARKNESS_THRESHOLD = 50 # TODO mess with this
 NAME_WHITELIST="--oem 0 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 NUM_WHITELIST="--oem 0 -c tessedit_char_whitelist=0123456789-/" #this whitelist can be passed when the input is expected to be a date
@@ -111,7 +111,7 @@ def alignImages(img, template):
 		templatePoints[i,:] = templateKeypoints[match.trainIdx].pt
 	
 	# find homography
-	h, mask = cv2.findHomography(imgPoints, templatePoints, cv2.RANSAC)
+	h, mask = cv2.findHomography(imgPoints, templatePoints, cv2.LEAST_MEDIAN)
 	
 	# apply homography, warping the image to appear as if it were directly below the camera.
 	height, width, channels = template.shape
@@ -182,6 +182,17 @@ def getText(roi, whitelist):
 	cv2.imwrite(SRC_PATH + "temp.jpg", roi)
 	result = pytesseract.image_to_string(Image.open(SRC_PATH + "temp.jpg"), lang='eng', config=whitelist)
 	os.remove(SRC_PATH + "temp.jpg")
+
+	#Fix to common OCR bug where tesseract reads '/' in dates as '1'.
+	if whitelist is NUM_WHITELIST:
+		result = result.replace(" ","").strip()
+		#if the line is 10 chars long, and there are 1s where slashes expected, replace the 1s at expected
+		#indeces with '/'
+		if len(result) is 10 and (result[2] == '1' or result[5] == '1'):		
+			s = list(result)
+			s[2] = '/'
+			s[5] = '/'
+			result = "".join(s)
 
 	return unidecode(result)
 #end of getText
@@ -268,19 +279,13 @@ def buildDocument(img):
 	
 
 #start of matchToTemplate
-#	This function takes an input BGR image, and a flag paramteter that is given by the function removeBorder(). When the flag is True,
-#	removeBorder modified the image by removing background. If the flag is false, removeBorder could not locate a document
-#	and returned the original image.. The program loops and searches through the templates folder, attempts to match a template
-#	to the image, and keeps track of the score of the match (a score of 1 is best). At the end of the loop, the
-#	function returns the best match it found, along with the state & orientation of the document (i.e. TX_V for
-#	a vertical TX ID).
-def matchToTemplate(img, background):
+#	TODO
+def matchToTemplate(img, background, location=SRC_PATH+"Templates/"):
 	h,w = img.shape[:2]
-
+	
 	#rotate document 90 degrees if needed.
 	#note: this rotation SHOULD be done in the face detection step, but if there is no face in the document, it can be done here.
 	img = correctOrientation(img)
-		
 	orientation = ""
 
 	#check if image still contains background. If it does not, we can figure out whether the document submitted is horizontal or
@@ -288,57 +293,93 @@ def matchToTemplate(img, background):
 	if not background:
 		h,w = img.shape[:2]
 		if h > w: #document is vertical
-			orientation = "_V_"
+			orientation = "_V"
 		elif w > h: #document is horizontal
-			orientation = "_H_"
+			orientation = "_H"
 
-	#perform prelim cleanup and edging	
-	grayImg = cleanImage(img)
-	grayImg = cv2.Canny(grayImg, 0, 150)
-	bestScore = 0
+	#Get the filename of the format that had the best match in the input image. the split() function gives the name of the file without
+	#the .jpg or .png extension.
+	form = multiScaleTemplateSelect(img, location).split('.')[0]
 
-	#loop through all the templates in the TEMPLATE src folder
-	for filename in os.listdir(SRC_PATH + "Templates/"):
-		#if we determined the orientation before the outer loop began, discard
-		#templates that are not oriented the same as the document.
-		if orientation is not "":
-			if not orientation in filename:
-				continue
-		if filename.endswith(".png") or filename.endswith(".jpg"): #All the templates expected to be jpg/png files
-			template = cv2.imread(SRC_PATH + "Templates/" + filename)			
-			grayTemplate = cleanImage(template)
-			grayTemplate = cv2.Canny(grayTemplate, 0, 150)
-
-			#make sure sized appropriately before pass to function, if template height/width > image height/width,
-			#cv2.matchTemplate will throw exception.
-			imgHeight, imgWidth = grayImg.shape
-			tempHeight, tempWidth = grayTemplate.shape
-
-			if(imgHeight > tempHeight):
-				grayImg = imutils.resize(grayImg, height=tempHeight - 10)
-			imgHeight, imgWidth = grayImg.shape
-
-			if(imgWidth > tempWidth):
-				grayImg = imutils.resize(grayImg, width=tempWidth - 10)
-
-			#Try to find matches between template and image using TM_SQDIFF matching algorithm
-			match = cv2.matchTemplate(grayImg, grayTemplate, cv2.TM_CCOEFF)
-			#grab the scores of the match object
-			minScore,maxScore,_,_ = cv2.minMaxLoc(match)
-
-			print("Filename: {}, score: {}".format(filename, maxScore))	
-			if maxScore > bestScore:
-				bestScore = maxScore
-				bestTemplate = template
-				#creates a substring from the filename up to the second underscore
-				#this isolates the state and orientation of the document
-				form = filename[:filename.index('_', filename.index('_') + 1)]	
-
+	print("Searching " + form + " directory...")	
 	
-	print("BEST MATCH: {}".format(form)) # TODO remove this
+	#update location to be the subdirectory containing all of the images for the specified form
+	location = location + form + "/"
+	#update form so that it will contain the filename and orientation if orientation was already determined
+	form += orientation	
+	
+	#check if the file we're looking for with the specified form and orientation exists.
+	if os.path.isfile(location + form + ".jpg"):
+		bestTemplate = cv2.imread(location + form + ".jpg")
+	elif os.path.isfile(location + form + ".png"):
+		bestTemplate = cv2.imread(location + form + ".png")
+	#if the first two branches of elif fail, then the file does not exist, that means one of two cases:
+	#1. outline of document was not found to assign a vertical or horizontal orientation
+	#2. Layout was not found (some states, such as oregon have different formats with DL picture on the left or right side.
+	#3. (most unlikely) false match to the state/template occurred in the first loop/
+	elif background is True:
+		#go into features subdirectory, this contains all the unique features for each license type. The best match will contain
+		#the form name in the filename. The form exists between the first underscore and the file extension. 
+		#ex filenames: feature1_V.jpg, feature3_H2.png
+		bestFeatureMatch = multiScaleTemplateSelect(img, location + "Features/")
+		form = form + "_" +  bestFeatureMatch.split("_")[1].split('.')[0]
+
+		#now that we have form name, attempt to read the CORRECT template from the /Templates/State/ folder.
+		bestTemplate = None
+		bestTemplate = cv2.imread(location + form + ".jpg")
+		if bestTemplate is None:
+			bestTemplate = cv2.imread(location + form + ".png")
+
+	print("FORM: {}".format(form))
 	return bestTemplate, form
 #end of matchToTemplate
 		
+
+#start of multiScaleTemplateSelect()TODO TODO TODO comment this code plz
+#	This function loops through all templates in the subdirectory, whose path is stored as a string in the variable named
+#	location. The function is also passed the image itself as an openCV object. The function loops over multiple scales
+#	of the input image, trying to match each template file in the subdirectory to the image. When the loop finishes, the filename
+#	of the image that best matched the input image is returned in the form of a string.
+def multiScaleTemplateSelect(img, location):
+	grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	grayImg = cv2.GaussianBlur(grayImg, (5,5), 0)
+	bestScore = 0
+	
+	#Loop through all files in the subdirectory stored in the variable location
+	for filename in os.listdir(location):
+		if filename.endswith(".png") or filename.endswith(".jpg"): #All the templates expected to be jpg/png files
+			template = cv2.imread(location + filename)
+			template = imutils.resize(template, height=40)
+			grayTemplate = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+			grayTemplate = cv2.GaussianBlur(grayTemplate, (5,5), 0)
+			(tH, tW) = template.shape[:2]
+
+			#Loop over different scales of the image
+			for scale in np.linspace(0.1, .5, 15)[::-1]:
+				resized = imutils.resize(grayImg, width=int(grayImg.shape[1] * scale))
+
+				#Break if the resized image is smaller than the template.	
+				if resized.shape[0] < tH or resized.shape[1] < tW:
+					break
+
+				#Edge detection
+				edgedImg = cv2.Canny(resized, 0, 200)
+				edgedTemplate = cv2.Canny(grayTemplate, 0, 200)
+				
+				#get list of matches, compare best match score to bestScore
+				result = cv2.matchTemplate(edgedImg, edgedTemplate, cv2.TM_CCORR_NORMED)
+				minScore,maxScore,_,_ = cv2.minMaxLoc(result)
+				
+				print("FILE: {}, SCORE: {}".format(filename, maxScore)) #TODO remove -- for debug
+
+				if maxScore > bestScore:
+					bestScore = maxScore
+					bestMatch = filename
+
+	print("BEST MATCH: {}, SCORE: {}".format(bestMatch, bestScore)) #TODO remove-- this was for debug
+	return bestMatch
+#end of multiScaleTemplateSelect
+
 
 #start of detectOrientation()
 #	This function determines the orientation of the text in an image so that if needed, we can rotate the image so that the
