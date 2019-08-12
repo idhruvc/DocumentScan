aedb89fc1a1c58eb4b673dfbcfcf1a05b80ef212
nicolasgover <47012483+nicolasgover@users.noreply.github.com>
2019-08-12 11:15:39 -0500

    Add files via upload

--- Document.py
+++ Document.py
@@ -2,12 +2,10 @@ import cv2
 import numpy as np
 import TemplateData as templates
 import imutils
-import pytesseract
 from unidecode import unidecode
-
-NAME_WHITELIST="--oem 0 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ-"
-NUM_WHITELIST="--oem 0 --psm 7 -c tessedit_char_whitelist=0123456789-/" #this whitelist can be passed when the input is expected to be a date
-ADDR_WHITELIST="--oem 0 --psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890-#"
+import io
+from google.cloud import vision
+import os
 
 
 #start of Document base class
@@ -38,32 +36,97 @@ class License(Document):
 		
 		#get coordinates of ROIS from the document template
 		
-		#retrieve name coordinates from template, get text from the image, process into the FN/LN fields based
+		#Figure out how to process the full name into the FN/LN fields based
 		#on how the name is formatted (order of fields, commas/not, number of lines, etc.)
-		box = getattr(templateData, "name")
 		nameFormat = getattr(templateData, "nameFormat")
 		#lambda to turn nameFormat into how many lines input is expected to be
 		lines = lambda l: int(l > 2) + 1
-		#add comma to OCR whitelist if the name is comma separated
-		if nameFormat is 1:
-			whitelist = NAME_WHITELIST + ","
-		else:
-			whitelist = NAME_WHITELIST
 
-		tH = image.shape[0]
-		tH_inches = getTemplateDimensionsInches(docType)[0]
-	
-		name = readROI(image[box[0][1]:box[1][1], box[0][0]:box[1][0]], whitelist, lines(nameFormat), tH, tH_inches)
-		self.last, self.first = parseName(name, nameFormat)
-		#retrieve dob coordinates from template, assign to object
+		#crop out each ROI by retrieving coordinates for each field from the template.
+		box = getattr(templateData, "name")
+		name = image[box[0][1]:box[1][1], box[0][0]:box[1][0]]
 		box = getattr(templateData, "dob")
-		self.dob = readROI(image[box[0][1]:box[1][1], box[0][0]:box[1][0]], NUM_WHITELIST, 1, tH, tH_inches)
-		#retrieve expiration coordinates from template, assign to object
+		dob = image[box[0][1]:box[1][1], box[0][0]:box[1][0]]
 		box = getattr(templateData, "expiration")
-		self.expiration = readROI(image[box[0][1]:box[1][1], box[0][0]:box[1][0]], NUM_WHITELIST, 1, tH, tH_inches)
-		#retrieve address coordinates from template, get text from image, assign to object
+		expiration = image[box[0][1]:box[1][1], box[0][0]:box[1][0]]
 		box = getattr(templateData, "address")
-		self.address = readROI(image[box[0][1]:box[1][1], box[0][0]:box[1][0]], ADDR_WHITELIST, 2, tH, tH_inches).replace('\n', ', ')
+		address = image[box[0][1]:box[1][1], box[0][0]:box[1][0]]
+
+		#add a white border to each image
+		name = cv2.copyMakeBorder(name, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(255,255,255))
+		dob = cv2.copyMakeBorder(dob, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(255,255,255))
+		expiration = cv2.copyMakeBorder(expiration, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(255,255,255))
+		address = cv2.copyMakeBorder(address, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(255,255,255))		
+
+		#compute width of the new image, resize each field so they stack evenly
+		maxWidth = max(name.shape[1], dob.shape[1], expiration.shape[1], address.shape[1])
+		name = imutils.resize(name, width=maxWidth)		
+		dob = imutils.resize(dob, width=maxWidth)
+		expiration = imutils.resize(expiration, width=maxWidth)
+		address = imutils.resize(address, width=maxWidth)	
+
+		#record the interval where we laid each region so that we can break up the response body.
+		nameRegion = (0, name.shape[0])
+		dobRegion = (nameRegion[1], nameRegion[1] + dob.shape[0])
+		expirationRegion = (dobRegion[1], dobRegion[1] + expiration.shape[0])
+		addressRegion = (expirationRegion[1], expirationRegion[1] + address.shape[0])	
+					
+		#concatenate each image on top of one another, resizing it to the maxWidth so that they stack perfectly
+		combined1 = np.concatenate((name, dob), axis=0)
+		combined2 = np.concatenate((expiration, address), axis=0)
+		combined = np.concatenate((combined1, combined2), axis=0)		
+		
+		#temp write to disk before requesting Cloud Vision API
+		tempFile = "/Users/ngover/Documents/TestPrograms/DocumentScan/test.png"
+		cv2.imwrite(tempFile, combined)
+		
+		cv2.imshow("Combined", imutils.resize(combined, height=700))
+		cv2.waitKey(0)		
+		
+		#start creating request
+		client = vision.ImageAnnotatorClient()
+		with io.open(tempFile, 'rb') as image_file:
+			content = image_file.read()
+			image = vision.types.Image(content=content)
+		response = client.text_detection(image=image)
+		labels = response.text_annotations
+	
+		#remove the temp file
+		os.remove(tempFile)		
+
+		nameText = ""
+		dobText = ""
+		expText = ""
+		addrText = ""
+		#go through each label, map each one to a field based on location
+		for label in labels[1:]:
+			center, text = processLabel(label)
+
+			#remove random hyphens
+			while text.endswith('-'):
+				text=text[:-1].strip()
+			while "--" in text:
+				text = text.replace("--", "")
+			
+			#separate response into different fields based on where we know it should have been located
+			if center[1] >=  nameRegion[0] and center[1] <= nameRegion[1]:
+				pass #ignore for now. We'll make a substring from the beginning of the string to the date
+				#so that newlines aren't lost.	
+			elif center[1] >= dobRegion[0] and center[1] <= dobRegion[1]:
+				dobText += unidecode(text)
+			elif center[1] >= expirationRegion[0] and center[1] <= expirationRegion[1]:
+				expText += unidecode(text)
+			elif center[1] >= addressRegion[0]:
+				addrText += unidecode(text) + " "
+		
+		body = labels[0].description
+		nameText = body[0:body.find(dobText.strip())]
+
+		#assign text to object
+		self.last, self.first = parseName(nameText.strip(), nameFormat)
+		self.dob = dobText.strip()
+		self.expiration = expText.strip()
+		self.address = addrText.strip()
 	#end of Driver's License Constructor
 
 	#Driver's License toString()
@@ -79,7 +142,7 @@ class License(Document):
 		temp = temp + 'ADDR:\t' + self.address + '\n'
 		return temp
 	#end of Driver's License toString()
-#end of SocialSecurity class	
+#end of Driver's License class	
 
 
 #start of SocialSecurity class
@@ -113,9 +176,9 @@ class SocialSecurity(Document):
 #end of SocialSecurity class
 
 
-
+#***
 #***THE REST OF THE FILE CONTAINS FUNCTIONS FOR THE CONSTRUCTORS TO HELP IN CREATING DOCUMENT OBJECTS FROM IMAGES***
-
+#***
 
 
 #start of documentFromImage
@@ -126,97 +189,12 @@ def documentFromImage(img, docType):
 	if docType.startswith("SSN"): #social security
 		document = SocialSecurity(img)
 	elif docType.startswith("PP"): #passport
-		document = None # todo
+		document = None # TODO create passport constructor
 	else: #driver's license
 		document = License(img, docType)
 	return document
 #end of documentFromImage
 
-#start of readROI
-#	This function is passed an image region of interest, or ROI, from which we will extract the text, and the height of the
-#	template image, which will be needed to calculate DPI to maximize OCR results. This method expects the image
-#	to only contain the data, because all text inside the ROI will be read and converted to a string, but only
-# 	for characters included in the whitelist that gets passed to the function. The calling function should know what
-#	kind of input to expect. If it is looking for a date/SSN/DL#/etc, it can pass a different whitelist than if it
-#	were looking for a name.
-def readROI(roi, whitelist, numLines, templateHeight, templateHeightInches):
-	regionBrightness = np.mean(roi)
-	#adjust region brightnes based on how bright the region is
-	if regionBrightness < 100:
-		beta = np.array([45.0])
-		cv2.add(roi, beta, roi)
-	elif regionBrightness > 100 and regionBrightness < 150:
-		beta = np.array([20.0])
-		cv2.add(roi, beta, roi)
-	elif regionBrightness > 150 and regionBrightness < 200:
-		beta = np.array([-20.0])
-		cv2.add(roi, beta, roi)
-	elif regionBrightness > 200:
-		beta = np.array([-45.0])
-		cv2.add(roi, beta, roi)
-	
-	#Set the DPI to 300 for the image we read
-	ratio = roi.shape[0] / float(templateHeight)
-	ratio = templateHeightInches * ratio
-	newHeight = int(ratio * 300)
-		
-	#optimize blur settings for OCR
-	if whitelist is NUM_WHITELIST and numLines == 1:
-		blurSize = (7,7)
-	elif numLines == 1:
-		blurSize = (13,13)
-	elif numLines == 2:
-		if whitelist is NAME_WHITELIST:
-			newHeight = int(newHeight * 2)
-		else:
-			newHeight = 135
-		blurSize = (13,13)
-	# prep image w/ resize, color convert, noise reduction
-	roi = imutils.resize(roi, height=newHeight)
-	roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY) 
-	roi = cv2.GaussianBlur(roi, blurSize, 0)
-
-	#increase contrast	
-	alpha = np.array([2.1])
-	cv2.multiply(roi, alpha, roi)
-
-	roi = cv2.adaptiveThreshold(roi, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 5) 
-
-	#dilation/erosion to pronounce features and reduce noise
-	kernel = np.ones((2,2), np.uint8)
-	roi = cv2.dilate(roi, kernel, iterations=1)
-	roi = cv2.erode(roi, kernel, iterations=2)	
-	
-	#add a white border to the image to make it easier for OCR
-	roi = cv2.copyMakeBorder(roi, 50, 50, 50, 50, cv2.BORDER_CONSTANT, value=255)
-	
-	#TODO remove imshow -- for debug
-	cv2.imshow("Text ROI", imutils.resize(roi, width=400))
-	cv2.waitKey(0)
-	
-	# retrieve text from the image
-	result = pytesseract.image_to_string(roi, config=whitelist)
-
-	#cleanup hyphens.. usually caused by noise in the image.
-	while result.endswith('-'):
-		result = result[:-1].strip()
-	while "--" in result: # TODO haven't tested this
-		result = result.replace("--", "")
-	
-	#Fix to common OCR bug where tesseract reads '/' in dates as '1'.
-	if whitelist is NUM_WHITELIST:
-		result = result.replace(" ","").replace("\n", "").strip()
-		#if the line is 10 chars long, and there are 1s where slashes expected, replace the 1s at expected
-		#indeces with '/'
-		if len(result) is 10 and (result[2] == '1' or result[5] == '1'):		
-			s = list(result)
-			s[2] = '/'
-			s[5] = '/'
-			result = "".join(s)
-	result = result.replace(" -", "")
-	
-	return unidecode(result)
-#end of readROI
 
 #start of parseName
 #	This function takes a string and the format the fields are in, then parses the string into
@@ -236,6 +214,12 @@ def parseName(name, nameFormat):
 			return "", name.strip()
 		elif nameFormat == 2: #LN, FN
 			names = name.split(",")
+			if len(names) == 1:
+				#sometimes comma misread as hyphen
+				names = name.split("-")
+			if len(names) == 1:
+				#commas can be misread as a period
+				names = name.split(".")
 			return names[0].strip().replace(",",""), names[1].strip().replace(",","")
 		elif nameFormat == 3: #LN\nFN
 			names = name.split("\n")
@@ -251,6 +235,23 @@ def parseName(name, nameFormat):
 #end of parseName
 
 
+#start of processLabel
+#	This function takes a response label from the Google Cloud Vision API and returns the text it contains as well
+#	as the center of the bounding box it was located in.
+def processLabel(label):
+	text = label.description
+	centerX = 0
+	centerY = 0		
+	
+	#calculate the center of the polygon
+	for vertex in label.bounding_poly.vertices:
+		centerX += vertex.x
+		centerY += vertex.y
+	centerX = centerX / len(label.bounding_poly.vertices)
+	centerY = centerY / len(label.bounding_poly.vertices)
+
+	return (centerX, centerY), text
+
 #start of getTemplateDimensionsInches
 #	Returns the size of a given doctype in inches. This number will be used to calculate the DPI for a doctype.
 #	Only expects to be passed the type of document/name of the template (string), returns H,W
@@ -264,4 +265,3 @@ def getTemplateDimensionsInches(docType):
 			return 2.215, 3.370
 	return 0,0
 #end of getTemplateDimensionsInches
-
